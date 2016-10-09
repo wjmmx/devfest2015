cd0744d71801751954530b96f3091c9241784d4f
philip.wang <philip.wang@perficientgdc.com.cn>
2016-10-09 08:12:29 +0800

    update checkin times

--- checkin.php
+++ checkin.php
@@ -9,11 +9,9 @@
 $v_qr = $_GET['qr'];
 echo $v_qr;
 
-
 include_once 'payease/util/conn.php';
 
 $sql_query = "select v_rcvname,v_rcvtel,v_email,rsg_role,checkin from payeaseinfo where qr='$v_qr'";
-//$sql_update= "update payeaseinfo set v_paymentdate='$v_paymentdate',v_pstatus='$v_pstatus' where v_oid='$v_oid'";
 
 if (!$result = $con->query($sql_query)) {
     // Oh no! The query failed.
@@ -47,4 +45,19 @@ echo "Your checkin status is: " . $checkin . "\n";
 $attendee_tel = $attendee['v_rcvtel'];
 echo "Your telphone # is: " . $attendee_tel . "\n";
 
+$checkin=$checkin+1;
+
+$sql_update= "update payeaseinfo set checkin='$checkin' where qr='$v_qr'";
+
+if (!$result = $con->query($sql_update)) {
+    // Oh no! The query failed.
+    echo "Sorry, the website is experiencing problems.";
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $con->errno . "\n";
+    echo "Error: " . $con->error . "\n";
+    exit;
+}
 ?>
