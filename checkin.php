344f801aa53ce0cd1e04b5839087afdcfcc4363b
philip.wang <philip.wang@perficientgdc.com.cn>
2016-10-09 09:19:26 +0800

    update checkin page

--- checkin.php
+++ checkin.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+    <title>Checkin页面</title>
+</head>
 <?php
 /**
  * Created by IntelliJ IDEA.
@@ -61,3 +67,13 @@ if (!$result = $con->query($sql_update)) {
     exit;
 }
 ?>
+<body>
+    <h3 align="center" >
+        欢迎 <?php echo $attendee_name ?>
+        </br>
+    </h3>
+    <h5 width="400" border="1" align="center" >
+        <?php echo $attendee_role ?>
+    </h5>
+</body>
+</html>
