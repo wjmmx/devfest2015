5e24b1b8fb4e3da85376d94c861780786c4978ff
philip.wang <philip.wang@perficientgdc.com.cn>
2016-10-10 22:46:46 +0800

    Change checkin page title

--- checkin.php
+++ checkin.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Checkin页面</title>
+    <title>SCRUM GATHERING CHINA 2016 - Checkin</title>
 </head>
 <?php
 /**
