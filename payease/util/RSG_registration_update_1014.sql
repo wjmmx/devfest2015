a117682dd7d3800f0cef974c5be1195ce1dd052c
philip.wang <philip.wang@perficientgdc.com.cn>
2016-10-15 22:11:04 +0800

    generating QR Code and send email on Oct. 15th. (new added 24 people)

---
+++ payease/util/RSG_registration_update_1014.sql
@@ -0,0 +1,8 @@
+/*
+These records corresponding people mailbox was wrong which led to mail post error
+ */
+UPDATE payeaseinfo SET v_email = '304749248@qq.com', v_rcvname = '周梅莲', qr_is_sent = 1 WHERE qr='xsvFmMNcJvMudxXk';
+UPDATE payeaseinfo SET v_email = 'miaoruijing@163.com', v_rcvname = '钱晨', qr_is_sent = 1 WHERE qr='JNetCjPMwTUBJEfp';
+UPDATE payeaseinfo SET v_email = 'XZhang210dc2@StateStreet.com', v_rcvname = '张晓辉', qr_is_sent = 1 WHERE qr='dDfqLAlUrxuHznzZ';
+UPDATE payeaseinfo SET v_email = 'wchen2@StateStreet.com', v_rcvname = '陈伟', qr_is_sent = 1 WHERE qr='aSUICgYobgemdJLz';
+UPDATE payeaseinfo SET v_email = 'lilizhan@cisco.com', v_rcvname = '张丽丽', qr_is_sent = 1 WHERE qr='pgzseVcTFArDfbuG';
